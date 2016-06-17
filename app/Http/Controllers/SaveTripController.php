86c64a8aea8771edb4df973dd389a3f0e851e2b5
Matthew Cunliffe <matthew.cunliffe12@imperial.ac.uk>
2016-06-17 10:58:50 -0400

    Check for valid login before saving trip

--- app/Http/Controllers/SaveTripController.php
+++ app/Http/Controllers/SaveTripController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Trip;
 use Carbon\Carbon;
+use DB;
+use App\AppLogin;
 
 class SaveTripController extends Controller {
 
@@ -14,19 +16,29 @@ class SaveTripController extends Controller {
 		if($request->has('hotel') and $request->has('route')
 				and $request->has('times_to_stay') and $request->has('waypoints')
 				and $request->has('transport_method') and $request->has('creator')
-				and $request->has('start_date_time')) {
+				and $request->has('start_date_time') and $request->has('app_id')
+				and $request->has('key')) {
 
-			$trip = new Trip;
-			$trip->start_date_time = $request->start_date_time;
-			$trip->hotel = $request->hotel;
-			$trip->waypoints = $request->waypoints;
-			$trip->times_to_stay = $request->times_to_stay;
-			$trip->route = $request->route;
-			$trip->transport_method = $request->transport_method;
-			$trip->creator = $request->creator;
-			$trip->save();
-			return "OK";
+			$login = DB::table('app_logins')->where('key', $request->key)
+					->where('app_id', $request->app_id)->first();
+			if($login != null and $login->valid_until < time()) {
 
+				//TODO: Start time is not being saved
+
+				$trip = new Trip;
+				$trip->start_date_time = $request->start_date_time;
+				$trip->hotel = $request->hotel;
+				$trip->waypoints = $request->waypoints;
+				$trip->times_to_stay = $request->times_to_stay;
+				$trip->route = $request->route;
+				$trip->transport_method = $request->transport_method;
+				$trip->creator = $login->user_id;
+				$trip->save();
+				return "OK";
+			}
+			else {
+				return "Invalid Login";
+			}
 
 		}
 		else {
